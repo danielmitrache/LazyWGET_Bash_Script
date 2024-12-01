df55b74026ff81e7a658cb9ee775b8b864f2ee27
daniel21 <mitracheadaniel@gmail.com>
2024-12-01 15:12:07 +0000

    Add files via upload
    Improvements:
    - added an "--absolute" flag
    - overall better functionality

--- lget.sh
+++ lget.sh
@@ -5,6 +5,11 @@ temp_links="temp_new_links"
 processed_links="processed_links"
 new_links="new_links"
 
+absolute_only=false
+if [[ $1 == "--absolute" ]]; then
+  absolute_only=true
+fi
+
 if [ ! -d LWGET_CONTENTS ]; then
   echo "Eroare: Nu există directorul LWGET_CONTENTS. Rulați mai întâi lwget cu un URL."
   exit 1
@@ -26,24 +31,45 @@ touch "$temp_links"
 
 while read -r link; do
   
+  if [[ "$link" == \#* ]]; then
+    echo "Link ignorat (ancoră): $link"
+    continue
+  fi
+
   if [[ "$link" != http* ]]; then
-    echo "Link relativ ignorat: $link"
+    echo "Link relativ incomplet: $link"
     continue
   fi
     
   wget --content-disposition -q "$link" -P "$base_dir"
   if [ $? -eq 0 ]; then
     last_file=$(ls -t "$base_dir" | head -n 1)
+
     if [ -s "$base_dir/$last_file" ]; then
-      grep -oP '(?<=href=")[^"]*' "$base_dir/$last_file" | grep -E '^https?://.+$' >> "$temp_links"
-      echo "$link" >> "$processed_links"
-      sed -i "\|^${link}$|d" "$new_links"
+
+      echo "Descarcare reusita: $link"
+
+      base_url=$(echo "$link" | grep -oP '^https?://[^/]+')
+      current_dir=$(dirname "$link")
+
+      while read -r extracted_link; do
+        if [[ "$extracted_link" == http* ]]; then
+          # echo "Link absolut: $extracted_link"
+          echo "$extracted_link" >> "$temp_links"
+        elif [[ "$extracted_link" == /* && "$absolute_only" == false ]]; then
+          # echo "Link relativ absolut completat: ${base_url}${extracted_link}"
+          echo "${base_url}${extracted_link}" >> "$temp_links"
+        elif [[ "$absolute_only" == false ]]; then
+          # echo "Link relativ la director completat: ${base_url}/${current_dir}/${extracted_link}"
+          echo "${base_url}/${current_dir}/${extracted_link}" >> "$temp_links"
+        fi
+      done < <(grep -oP '(?<=href=")[^"]*' "$base_dir/$last_file")
+
     else
       echo "Descărcare eșuată (fișier gol): $link"
       rm "$base_dir/$last_file"
       continue
     fi
-    echo "Descarcare reusita: $link"
   else
     echo "Eroare la descărcarea link-ului: $link"
   fi
--- lwget.sh
+++ lwget.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-  echo "Utilizare: lwget <URL>"
+absolute_only=false
+
+if [[ ( "$#" -lt 1 || "$#" -gt 2) ]]; then
+  echo "Utilizare: lwget (<flag>) <URL>"
   exit 1
+fi
+
+input_site=$1
+if [ "$#" -eq 2 ]; then
+  if [[ "$1" == "--absolute" ]]; then
+    absolute_only=true
+    input_site=$2
+  fi
 fi
 
 mkdir LWGET_CONTENTS
 cd LWGET_CONTENTS
 
-input_site=$1
 base_dir="downloads"
+
+if [[ !("$input_site" =~ ^https?://\.*) ]]; then
+  protocol=$(wget -nv "$input_site" 2>&1 | grep -oP "(?<=URL:)https?")
+  input_site="${protocol}://${input_site}"
+fi
 
 touch new_links
 touch processed_links
@@ -22,12 +36,27 @@ file_name=$(ls -t "$base_dir" | head -n 1)
 
 if [ ! -f "$base_dir/$file_name" ]; then
   echo "Eroare: Nu s-a descărcat fișierul HTML de la $input_site."
+  rm -r ../LWGET_CONTENTS
   exit 2
 fi
 
-echo "Fișierul HTML descărcat este: $base_dir/$file_name"
+echo "Fișierul HTML descărcat este: LWGET_CONTENTS/$base_dir/$file_name"
 
-grep -oP '(?<=href=")[^"]*' "$base_dir/$file_name" > new_links
-echo "Link-urile au fost salvate în fișierul new_links."
+current_dir=$(dirname "$input_site")
+
+while read -r extracted_link; do
+  if [[ "$extracted_link" == http* ]]; then
+    # echo "Link absolut: $extracted_link"
+    echo "$extracted_link" >> new_links
+  elif [[ "$extracted_link" == /* && "$absolute_only" == false ]]; then
+    # echo "Link relativ absolut completat: ${base_url}${extracted_link}"
+    echo "${input_site}${extracted_link}" >> new_links
+  elif [[ "$absolute_only" == false ]]; then
+    # echo "Link relativ la director completat: ${base_url}/${current_dir}/${extracted_link}"
+    echo "${input_site}/${current_dir}/${extracted_link}" >> new_links
+  fi
+done < <(grep -oP '(?<=href=")[^"]*' "$base_dir/$file_name")
+
+echo "Link-urile au fost salvate în fișierul LWGET_CONTENTS/new_links."
 
 exit 0
